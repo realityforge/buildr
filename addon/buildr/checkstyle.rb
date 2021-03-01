c47f9448b950b2dafd6a619bce28533912d888b5
Peter Donald <peter@realityforge.org>
2021-03-01 13:19:15 +1100

    Prefer the -all dependency

--- addon/buildr/checkstyle.rb
+++ addon/buildr/checkstyle.rb
@@ -19,23 +19,12 @@ module Buildr
   module Checkstyle
 
     class << self
-
-      # The specs for requirements
-      def dependencies
-        %w(
-            com.puppycrawl.tools:checkstyle:jar:6.12.1
-            org.antlr:antlr4-runtime:jar:4.5.1-1
-            antlr:antlr:jar:2.7.7
-            com.google.guava:guava:jar:18.0 org.apache.commons:commons-lang3:jar:3.4
-            org.abego.treelayout:org.abego.treelayout.core:jar:1.0.1
-            commons-cli:commons-cli:jar:1.3
-            commons-beanutils:commons-beanutils-core:jar:1.8.3
-            commons-logging:commons-logging:jar:1.1.1
-          )
-      end
-
       def checkstyle(configuration_file, format, output_file, source_paths, options = {})
-        dependencies = self.dependencies + (options[:dependencies] || [])
+        version = '8.40'
+        artifact = Buildr.artifact("com.puppycrawl.tools:checkstyle-all:jar:#{version}")
+        Buildr.download(artifact => "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-#{version}/checkstyle-#{version}-all.jar")
+
+        dependencies = [artifact] + (options[:dependencies] || [])
         cp = Buildr.artifacts(dependencies).each { |a| a.invoke if a.respond_to?(:invoke) }.map(&:to_s)
 
         args = []
