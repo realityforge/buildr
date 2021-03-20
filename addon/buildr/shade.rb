7a807da4c67f2e161609fb8b2ce0e930929e3c4a
Peter Donald <peter@realityforge.org>
2021-03-20 21:04:25 +1100

    Remove unused config

--- addon/buildr/shade.rb
+++ addon/buildr/shade.rb
@@ -15,27 +15,7 @@
 module Buildr
   # Provides the shade method.
   module Shade
-
     class << self
-
-      # The specs for requirements
-      def dependencies
-        %w(
-          net.sourceforge.pmd:pmd-core:jar:6.11.0
-          net.sourceforge.pmd:pmd-java:jar:6.11.0
-          net.sourceforge.pmd:pmd-java8:jar:6.11.0
-          jaxen:jaxen:jar:1.1.6
-          commons-io:commons-io:jar:2.6
-          com.beust:jcommander:jar:1.72
-          org.ow2.asm:asm:jar:7.1
-          com.google.code.gson:gson:jar:2.8.5
-          net.java.dev.javacc:javacc:jar:5.0
-          net.sourceforge.saxon:saxon:jar:9.1.0.8
-          org.apache.commons:commons-lang3:jar:3.8.1
-          org.antlr:antlr4-runtime:jar:4.7
-        )
-      end
-
       def shade(input_jar, output_jar, relocations = {})
 
         shaded_jar = (input_jar.to_s + '-shaded')
